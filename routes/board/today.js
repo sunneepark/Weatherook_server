5376e517f297d6887ce5ce579b20a073ba6e5c44
jiwon <shark.cametotea@gmail.com>
2018-07-05 01:50:09 +0900

    0705 jiwon commit1

---
+++ routes/board/today.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../module/pool.js'); 
+const upload = require('../../config/multer.js'); 
+const moment = require('moment');
+const jwt = require('../../module/jwt.js');
+
+router.get('/', async function(req, res){
+    
+}); 
