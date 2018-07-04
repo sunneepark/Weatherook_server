778563372356c45143c6303a283366a2aa4faa06
sunneepark <hl2ozs@naver.com>
2018-07-04 19:06:34 +0900

    auth completed

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/signup', require('./signup.js'))
+router.use('/', require('./signup.js'))
 router.use('/signin', require('./signin.js'))
 router.use('/', require('./delete.js'))
-
-
 
 module.exports = router;
