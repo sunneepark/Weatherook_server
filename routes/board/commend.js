270e8cedd74ee36faa9478fd00d93319ae6130db
sunneepark <hl2ozs@naver.com>
2018-07-11 05:35:51 +0900

    commend_completed

--- routes/board/commend.js
+++ routes/board/commend.js
@@ -7,7 +7,6 @@ const jwt = require('../../module/jwt.js');
 //추천게시물 보여주기
 router.post('/', async function(req, res){
     let token = req.headers.token;
-
     let weather_weather=req.body.weather_weather;
     let weather_temp = parseInt(req.body.weather_temp,10);
 
@@ -28,8 +27,8 @@ router.post('/', async function(req, res){
         let weatherQuery='SELECT commend_idx, commend_img FROM board_commend WHERE commend_temp_min <= ? and commend_temp_max > ? and commend_weather= ? and commend_check=0 order by rand() limit 4';
         let weatherResult = await db.queryParam_Arr(weatherQuery, [weather_temp, weather_temp, weather_weather]);
 
-        weatherQuery='UPDATE board_commend SET commend_check=0';
-        weatherResult = await db.queryParam_Arr(weatherQuery);
+        let weatherupdateQuery='UPDATE board_commend SET commend_check=0';
+        let weatherupdateResult = await db.queryParam_None(weatherupdateQuery);
         if (!weatherResult){ //쿼리 에러
             res.status(500).send({
                 message : "Internal Server Error1"
