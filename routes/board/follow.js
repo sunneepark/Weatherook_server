eeff55471bd649c79088cdaef1249660c49584ff
sunneepark <hl2ozs@naver.com>
2018-07-12 05:39:53 +0900

    follower_comleted

--- routes/board/follow.js
+++ routes/board/follow.js
@@ -16,9 +16,15 @@ router.post('/', async function(req, res){
         let user_idx=decoded.user_idx;
         let data_res;
         let data_result=[];
+        let checkfollow='SELECT follower_idx FROM follow where user_idx= ?';
+        let checkresult=await db.queryParam_Arr(checkfollow,[user_idx]);
+        if(checkresult.length == 0){
+            res.status(201).send({
+                message : "user have not followers"
+            }); 
+        }
         let checkfollowQuery='select board_idx from board where board_auth="PUBLIC" and board_idx in(select board_idx from user_board where user_idx in (SELECT follower_idx FROM follow where user_idx= ? )) order by board_date desc';
         let checkfollowResult = await db.queryParam_Arr(checkfollowQuery, [user_idx]);
-        console.log(checkfollowResult);
         if(!checkfollowResult){ // 쿼리 에러
             res.status(500).send({
                 message : "Internal Server Error"
