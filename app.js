1fe39606a4536e0c2cf8b80957f62316ce1c58d6
HyunnSangCho <zx251874@naver.com>
2018-07-13 08:18:45 +0900

    swagger-commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const swaggerDefinition = {
     version: '1.0.0', // Version (required)
     description: 'weatherook', // Description (optional)
   },
-  host: 'https://weatherook.cf', // Host (optional)
+  host: 'weatherook.cf', // Host (optional)
   basePath: '/', // Base path (optional)
 }
 
